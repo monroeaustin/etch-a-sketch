456b2699ef502b014a7b8407a906d91f44d2e7a3
Monroe Austin <monroeraustin2@yahoo.com>
2024-02-24 00:09:46 -0800

    Implemented Reset Button w UI Changes

--- java.js
+++ java.js
@@ -1,32 +1,55 @@
 container = document.querySelector('.cntr')
+const resetbtn = document.getElementById('resetbtn')
+const allDivs = document.querySelectorAll('.allDivs')
 
+// All Global Variable Elements Above
 
+function resetGame(){
+  // let askGridAmount = prompt("Please choose the grid amount Number x Number (ie: 16x16)")
+  // askGridAmount
+  // let b = parseInt(askGridAmount)
+  // document.body.appendChild(container)
+  createGrid(16)
+  console.log('This shouldve created a new game.')
+}
 
 function createGrid(b) {
 
 
-    for (let i = 0;i < b; ++i){
+   // Auto Size Container Based on Entry gridEntry x 100 = maxHeightnWidth
+    c = b * b
+    maxHeightnWid = b * 100
+    let pixelHeight = maxHeightnWid + "px"
+    let pixelwidth = maxHeightnWid + "px"
+    container.style.maxHeight = pixelHeight
+    container.style.maxWidth = pixelwidth
+    container.style.display = "flex";
+
+
+    for (let i = 0;i < c; ++i){
 
         // Regular class added for Default Styling
         const newDiv = document.createElement('div');
         newDiv.classList.add('regular');
+        newDiv.classList.add('allDivs');
         
         // Typical Styling Default CSS within JS/DOM
         newDiv.innerText = "testing";
         newDiv.style.height = "100px";
         newDiv.style.width = "100px";
         newDiv.style.border = "solid 1px black";
-        container.style.display = "flex";
-
-      
-
+        
         container.appendChild(newDiv)
     
-    } 
-    // After creation --> Simply added eventListener to all Items
-    const allDivs = document.querySelectorAll('.regular')
+    }  
 
-    allDivs.forEach(function(div) {
+   
+
+    
+    // After creation --> Simply added eventListener to all Items
+    const regDivs = document.querySelectorAll('.regular')
+
+    regDivs.forEach(function(div) {
         div.addEventListener('mouseover', function() {
           div.classList.remove('regular')
           div.classList.add('hovered')
@@ -36,4 +59,18 @@ function createGrid(b) {
       });
     }
 
-createGrid(256);
+// Features for Reset Button
+
+resetbtn.addEventListener('click', function(){
+    function removeAllChildNodes(parent){
+      while (parent.firstChild) {parent.removeChild(parent.firstChild)}
+    }
+
+    removeAllChildNodes(container)
+
+    createGrid(16)
+
+    // createGrid(prompt("Please choose your layout"))
+})
+
+createGrid(6)
